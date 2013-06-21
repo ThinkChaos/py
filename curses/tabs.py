fd1601d9fba7f53f19bdbfe5d03563c24224a1a9
ThinkChaos <ThinkChaos@users.noreply.github.com>
2013-06-21 23:22:18 +0300

    Added tab-bar

--- curses/tabs.py
+++ curses/tabs.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+# coding: utf8
 
 from os import remove
 from glob import glob
@@ -22,6 +23,26 @@ def save(tab, f):
   f = open(f, 'w')
   return tab.putwin(f)
 
+def border(tab, active=False, first=False):
+  # curses.border(left, right, top, bottom, top-left, top-right, bottom-left, bottom-right)
+  # 0 is default
+  if active:
+    if first:                                                                 # ┌───────┐
+      tab.border(0, 0, 0, ' ', 0, 0, curses.ACS_VLINE, curses.ACS_LLCORNER)   # │ Tab 1 │
+                                                                              # │       └
+
+    else:                                                                     # ┌───────┐
+      tab.border(0, 0, 0, ' ', 0, 0, curses.ACS_LRCORNER, curses.ACS_LLCORNER)# │ Tab X │
+                                                                              # ┘       └
+  else:
+    if first:                                                                 # ┌───────┐
+      tab.border(0, 0, 0, 0, 0, 0, curses.ACS_ULCORNER, curses.ACS_HLINE)     # │ Tab 1 │
+                                                                              # ┌────────
+
+    else:                                                                     # ┌───────┐
+      tab.border(0, 0, 0, 0, 0, 0, curses.ACS_HLINE, curses.ACS_HLINE)        # │ Tab X │
+                                                                              # ─────────
+
 def help(s, key_next, key_prev, key_jump, key_help, key_quit, tab_number):
   s.addstr(0, 0, 'Next tab:       %s' % key_next)
   s.addstr(1, 0, 'Previous tab:   %s' % key_prev)
@@ -38,7 +59,7 @@ def main(stdscr):
   curses.use_default_colors()
 
   # Configuration
-  tab_number = 5
+  tab_number = 10
   key_next = '+'
   key_prev = '-'
   key_jump = ':'
@@ -49,27 +70,54 @@ def main(stdscr):
   tabs, files = ([], [])
   d = os.path.dirname(__file__)
 
-  # Set up each tab and associated file
+  maxy, maxx = stdscr.getmaxyx()
+  h, w = (maxy - 4, maxx - 2)
+  # Set up each pane and associated file
   for i in range(0, tab_number):
-    t = curses.newwin(0, 0)
+    t = curses.newwin(h, w, 3, 1)
     f = os.path.join(d, '%i.ctab' % i)
     tabs  += [t]
     files += [f]
 
     # Initalize the tab
-    t.box()
-    t.addstr(1, 1, 'You are in tab %i' % (i + 1))
+    t.addstr('You are in tab %i' % (i + 1))
 
     # Save tab to file
     save(t, files[i])
 
+  # Set up each tab
+  bar, x = ([], 0)
+  for i in range(0, tab_number):
+    txt = 'Tab %i' % (i + 1)
+    w = len(txt) + 4
+    t = curses.newwin(3, w, 0, x)
+    border(t, active=False)
+    t.addstr(1, 2, txt)
+    t.refresh()
+    bar += [t]
+    x += w
+    a = False
+    f = False
 
-  help(stdscr, key_next, key_prev, key_jump, key_help, key_quit, tab_number)
+  frame = curses.newwin(maxy - 3, maxx, 3, 0)
+  frame.border(0, 0, 0, 0, curses.ACS_VLINE, curses.ACS_VLINE)
+  frame.refresh()
+
+  end = curses.newwin(1, maxx - x, 2, x)
+  end.border(' ', ' ', ' ', 0, ' ', ' ', curses.ACS_HLINE, curses.ACS_URCORNER)
+  end.refresh()
 
   n = 0 # Start at tab 0
+  curses.ungetch(key_help) # Simulate press of key_help
   while True:
     # Load and display new tab
+    first = False
+    if n == 0:
+      first = True
+    border(bar[n], active=True, first=first)
+    bar[n].addstr(1, 2, 'Tab %i' % (n + 1), curses.A_BOLD)
     t = load(files[n])
+    bar[n].refresh()
     t.refresh()
 
     o = n # Save current tab ID
@@ -87,21 +135,23 @@ def main(stdscr):
 
       elif k == key_jump:
         try:
-          t.addstr(4, 1, ' ' * (curses.COLS - 2)) # Empty line to prevenet overlapping text
-          t.addstr(4, 1, 'Jump to tab: ')
-          k = int(t.getkey()) - 1 # This is the reason for try (might not be int)
-          t.addstr(4, 1, ' ' * (curses.COLS - 2))
+          t.addstr(2, 0, ' ' * (curses.COLS - 2)) # Empty line to prevenet overlapping text
+          t.addstr(2, 0, 'Jump to tab: ')
+          curses.echo(1) # Show what's being typed
+          k = int(t.getstr()) - 1 # This is the reason for try (might not be int)
+          curses.echo(0)
+          t.addstr(2, 0, ' ' * (curses.COLS - 2))
           if (k != n) and (k in range(tab_number)):
             n = k
             break
         except:
-          t.addstr(4, 1, ' ' * (curses.COLS - 2))
+          t.addstr(2, 0, ' ' * (curses.COLS - 2))
 
       elif k == key_help:
-        save(t, f)
+        save(t, files[n])
         t.erase()
         help(t, key_next, key_prev, key_jump, key_help, key_quit, tab_number)
-        t = load(f)
+        t = load(files[n])
 
       elif k == key_quit:
         quit(files)
@@ -109,6 +159,11 @@ def main(stdscr):
     # This isn't needed in this example, but in a real
     # use where the tab is modified in between it would
     save(t, files[o])
+    if o == 0:
+      first = True
+    border(bar[o], first=first)
+    bar[o].addstr(1, 2, 'Tab %i' % (o + 1))
+    bar[o].refresh()
 
 
 if __name__ == '__main__':
